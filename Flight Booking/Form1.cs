f1a5c12eb29ce8475272bdde9a626793cfb7c17a
Kadir Hali <kadirhali07@gmail.com>
2023-06-06 13:14:06 +0300

    Add project files.

---
+++ Flight Booking/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flight_Booking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Rota: " + comboBox1.Text + " - " + comboBox2.Text);
+            listBox1.Items.Add("Tarih ve Saat: " + dateTimePicker1.Text + "-" + maskedTextBox1.Text);
+            listBox1.Items.Add("Yolcu Ad Soyad: " + textBox1.Text);
+            listBox1.Items.Add("Yolcu Telefon ve TC :" + maskedTextBox3.Text + "-" + maskedTextBox2.Text);
+            MessageBox.Show("Yolcu Başarıyla Kaydedildi!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label9.Text = comboBox2.Text;
+            comboBox2.Text = comboBox1.Text;
+            comboBox1.Text = label9.Text;
+        }
+    }
+}
